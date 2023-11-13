32769bf9d5a215b35734ec0102651b2291b27060
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-14 00:56:35 +0100

    feat(Command): added the implementation for the userinterface commands

--- src/main/java/edu/ntnu/stud/commands/Command.java
+++ src/main/java/edu/ntnu/stud/commands/Command.java
@@ -1,2 +1,13 @@
-package edu.ntnu.stud.commands;public interface Command {
+package edu.ntnu.stud.commands;
+
+@FunctionalInterface
+public interface Command {
+    void execute();
+    default String getName(){
+      return "Command";
+    }
+
+    default String getDescription(){
+      return "Description of a command";
+    }
 }
