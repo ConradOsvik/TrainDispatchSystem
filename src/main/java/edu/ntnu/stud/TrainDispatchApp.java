93c6f74b7c0983fe907bf52d464a04b7e42e7260
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-14 00:55:38 +0100

    feat(TrainDispatchApp): added the basic methods of TrainDispatchApp

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -1,8 +1,17 @@
 package edu.ntnu.stud;
 
+import edu.ntnu.stud.models.TrainDeparture;
+
 /**
  * This is the main class for the train dispatch application.
  */
 public class TrainDispatchApp {
-  // TODO: Fill in the main method and any other methods you need.
+  private static void init(){}
+
+  private static void start(){}
+
+  public static void main(String[] args) {
+    TrainDeparture trainDeparture = new TrainDeparture("12:00", "F1", 100, "Oslo", 1, "00:10");
+    System.out.println(trainDeparture.toString());
+  }
 }
