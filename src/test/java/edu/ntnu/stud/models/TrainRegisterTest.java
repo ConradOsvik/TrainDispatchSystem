af6f24757a5e1ec3dc27693b92fb5fa2eaa02463
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-14 01:12:26 +0100

    test: added initialÃ setup for the tests of TrainRegister model

--- src/test/java/edu/ntnu/stud/models/TrainRegisterTest.java
+++ src/test/java/edu/ntnu/stud/models/TrainRegisterTest.java
@@ -1,2 +1,13 @@
-package edu.ntnu.stud.models;public class TrainRegisterTest {
+package edu.ntnu.stud.models;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TrainRegisterTest {
+  TrainRegister trainRegister;
+
+  @BeforeEach
+  public void setup(){
+    trainRegister = new TrainRegister();
+    trainRegister.addTrain(new TrainDeparture("12:00", "F1", 100, "Oslo", 1, "00:10"));
+  }
 }
