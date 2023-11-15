b02264b686c037aee5e9b42fece31f5cbd408a41
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-15 15:29:12 +0100

    feat(TrainController): added and started working on the main controller handling the train system

--- src/main/java/edu/ntnu/stud/controllers/TrainController.java
+++ src/main/java/edu/ntnu/stud/controllers/TrainController.java
@@ -1,2 +1,15 @@
-package edu.ntnu.stud.controllers;public class TrainController {
+package edu.ntnu.stud.controllers;
+
+import edu.ntnu.stud.models.TrainRegister;
+import edu.ntnu.stud.views.ConsoleView;
+
+public class TrainController {
+  private TrainRegister trainRegister;
+  private ConsoleView consoleView;
+
+  public TrainController(TrainRegister trainRegister, ConsoleView consoleView){
+    this.trainRegister = trainRegister;
+    this.consoleView = consoleView;
+
+  }
 }
