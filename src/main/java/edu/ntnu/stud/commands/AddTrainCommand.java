7d6c72e9c5eaced29672a025844843e6a1697724
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-15 15:28:31 +0100

    feat(addTrainCommand): started on a very basic command implementing the command interface

--- src/main/java/edu/ntnu/stud/commands/AddTrainCommand.java
+++ src/main/java/edu/ntnu/stud/commands/AddTrainCommand.java
@@ -1,2 +1,19 @@
-package edu.ntnu.stud.commands;public class AddTrainCommand {
+package edu.ntnu.stud.commands;
+
+public class AddTrainCommand implements Command {
+
+  @Override
+  public String getName() {
+    return "Add train";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Add a train to the train register";
+  }
+
+  @Override
+  public void execute() {
+    System.out.println("AddTrainCommand executed");
+  }
 }
