49ed219152a4c9ef27814ce7c1687cf9a6eeaeb2
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-15 15:46:53 +0100

    feat(ExceptionHandling): started on custom exceptions for different format and logic errors

--- src/main/java/edu/ntnu/stud/exceptions/InvalidNumberException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidNumberException.java
@@ -1,5 +1,13 @@
 package edu.ntnu.stud.exceptions;
 
 public class InvalidNumberException extends Exception{
+    private static final String ERROR_MESSAGE = "The number you provided is invalid, please try again.";
 
+    public InvalidNumberException(){
+      super(ERROR_MESSAGE);
+    }
+
+    public InvalidNumberException(String message){
+        super(message);
+    }
 }
