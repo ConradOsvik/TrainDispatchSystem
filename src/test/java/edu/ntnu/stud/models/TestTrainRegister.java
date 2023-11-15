080d15aed6a2950b5eb52894a19211875c605d53
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-15 15:33:13 +0100

    fix(TestTrainRegister): reformatted file

--- src/test/java/edu/ntnu/stud/models/TestTrainRegister.java
+++ src/test/java/edu/ntnu/stud/models/TestTrainRegister.java
@@ -2,12 +2,26 @@ package edu.ntnu.stud.models;
 
 import org.junit.jupiter.api.BeforeEach;
 
+/**
+ * Test class for the TrainRegister entity class. The class methods follows the
+ * methodName_stateUnderTest_expectedBehavior naming convention.
+ */
 public class TestTrainRegister {
+
   TrainRegister trainRegister;
 
   @BeforeEach
-  public void setup(){
+  public void setup() {
     trainRegister = new TrainRegister();
-    trainRegister.addTrain(new TrainDeparture("12:00", "F1", 100, "Oslo", 1, "00:10"));
+    trainRegister.addTrain(
+        new TrainDeparture(
+            "12:00",
+            "F1",
+            100,
+            "Oslo",
+            1,
+            "00:10"
+        )
+    );
   }
 }
