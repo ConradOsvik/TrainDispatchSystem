156e56479eacaf7c1cd804c959ecd286a3f3d240
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-17 02:00:34 +0100

    feat(Exceptions): added a logic and format input exception

--- src/main/java/edu/ntnu/stud/exceptions/InvalidFormatException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidFormatException.java
@@ -1,2 +1,13 @@
-package edu.ntnu.stud.exceptions;public class InvalidFormatException {
+package edu.ntnu.stud.exceptions;
+
+public class InvalidFormatException extends Exception {
+  private static final String ERROR_MESSAGE = "The input you provided has an invalid format, please try again.";
+
+  public InvalidFormatException(){
+    super(ERROR_MESSAGE);
+  }
+
+  public InvalidFormatException(String message){
+    super(message);
+  }
 }
--- src/main/java/edu/ntnu/stud/exceptions/InvalidInputException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidInputException.java
@@ -1,2 +1,13 @@
-package edu.ntnu.stud.exceptions;public class InvalidInputException {
+package edu.ntnu.stud.exceptions;
+
+public class InvalidInputException extends Exception {
+  private static final String ERROR_MESSAGE = "The input you provided is invalid, please try again.";
+
+  public InvalidInputException(){
+    super(ERROR_MESSAGE);
+  }
+
+  public InvalidInputException(String message){
+    super(message);
+  }
 }
