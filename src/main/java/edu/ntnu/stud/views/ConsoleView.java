0e011b450e19ddc16d00251edcdee277194c4e04
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-17 01:59:46 +0100

    feat(ConsoleView): started working on a displayMenu and getUserInput method

--- src/main/java/edu/ntnu/stud/views/ConsoleView.java
+++ src/main/java/edu/ntnu/stud/views/ConsoleView.java
@@ -1,5 +1,26 @@
 package edu.ntnu.stud.views;
 
+import edu.ntnu.stud.commands.Command;
+import edu.ntnu.stud.input.InputValidator;
+import java.util.List;
+
 public class ConsoleView {
 
+  private final InputValidator inputValidator;
+
+  public ConsoleView() {
+    this.inputValidator = new InputValidator();
+  }
+
+  public void displayMenu(List<Command> commands) {
+    System.out.println("Please choose a command:");
+    for (int i = 0; i < commands.size(); i++) {
+      System.out.println(
+          (i + 1) + ": " + commands.get(i).getName() + " - " + commands.get(i).getDescription());
+    }
+  }
+
+  public String getUserInput() {
+    return inputValidator.getTime();
+  }
 }
