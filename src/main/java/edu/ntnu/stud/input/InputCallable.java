03dcddedf1c88b27fda1efad7abae78a316d0a06
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-17 01:58:59 +0100

    feat(Input): added an input validator that runs until valid input, together with an interface describing a generic input method

--- src/main/java/edu/ntnu/stud/input/InputCallable.java
+++ src/main/java/edu/ntnu/stud/input/InputCallable.java
@@ -1,2 +1,10 @@
-package edu.ntnu.stud.input;public interface InputCallable {
+package edu.ntnu.stud.input;
+
+import edu.ntnu.stud.exceptions.InvalidFormatException;
+import edu.ntnu.stud.exceptions.InvalidInputException;
+
+@FunctionalInterface
+public interface InputCallable<T> {
+
+  T call() throws InvalidFormatException, InvalidInputException;
 }
--- src/main/java/edu/ntnu/stud/input/InputValidator.java
+++ src/main/java/edu/ntnu/stud/input/InputValidator.java
@@ -1,2 +1,28 @@
-package edu.ntnu.stud.input;public class InputValidator {
+package edu.ntnu.stud.input;
+
+import edu.ntnu.stud.exceptions.InvalidFormatException;
+import edu.ntnu.stud.exceptions.InvalidInputException;
+import java.util.concurrent.Callable;
+
+public class InputValidator {
+
+  private final InputHandler inputHandler;
+
+  public InputValidator() {
+    this.inputHandler = new InputHandler();
+  }
+
+  public String getTime() {
+    return execute(inputHandler::getTime);
+  }
+
+  private <T> T execute(InputCallable<T> callable) {
+    while (true) {
+      try {
+        return callable.call();
+      } catch (InvalidFormatException | InvalidInputException e) {
+        System.out.println(e.getMessage());
+      }
+    }
+  }
 }
