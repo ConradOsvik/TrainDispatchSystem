9414353967bcc3debc71475f3f2b02ca026bb9e3
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-17 01:57:11 +0100

    feat(Input): added an input handler for complex inputs (time, track, etc)

--- src/main/java/edu/ntnu/stud/input/InputHandler.java
+++ src/main/java/edu/ntnu/stud/input/InputHandler.java
@@ -1,2 +1,17 @@
-package edu.ntnu.stud.input;public class InputHandler {
+package edu.ntnu.stud.input;
+
+import edu.ntnu.stud.exceptions.InvalidFormatException;
+import edu.ntnu.stud.exceptions.InvalidInputException;
+
+public class InputHandler {
+
+  private final PrimitiveInputHandler primitiveInputHandler;
+
+  public InputHandler() {
+    this.primitiveInputHandler = new PrimitiveInputHandler();
+  }
+
+  public String getTime() throws InvalidFormatException, InvalidInputException {
+    return primitiveInputHandler.getString();
+  }
 }
