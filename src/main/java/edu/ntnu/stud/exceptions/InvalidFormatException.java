df4a4497b583101fe66867fd4b7c2f965facb6ec
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 03:04:56 +0100

    fix(exceptions): fixed colon

--- src/main/java/edu/ntnu/stud/exceptions/InvalidFormatException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidFormatException.java
@@ -1,7 +1,7 @@
 package edu.ntnu.stud.exceptions;
 
 public class InvalidFormatException extends Exception {
-  private static final String ERROR_MESSAGE = "The input you provided has an invalid format, please try again.";
+  private static final String ERROR_MESSAGE = "The input you provided has an invalid format, please try again:";
 
   public InvalidFormatException(){
     super(ERROR_MESSAGE);
--- src/main/java/edu/ntnu/stud/exceptions/InvalidInputException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidInputException.java
@@ -1,7 +1,7 @@
 package edu.ntnu.stud.exceptions;
 
 public class InvalidInputException extends Exception {
-  private static final String ERROR_MESSAGE = "The input you provided is invalid, please try again.";
+  private static final String ERROR_MESSAGE = "The input you provided is invalid, please try again:";
 
   public InvalidInputException(){
     super(ERROR_MESSAGE);
