f42877e2e3d4c0f094a59578815261ebaa049690
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:47:56 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/commands/Command.java
+++ src/main/java/edu/ntnu/stud/commands/Command.java
@@ -1,15 +1,31 @@
 package edu.ntnu.stud.commands;
 
+/**
+ * A functional interface representing the baseline of a command.
+ */
 @FunctionalInterface
 public interface Command {
 
+  /**
+   * A getter for the name of the command.
+   *
+   * @return the name of the command
+   */
   default String getName() {
     return "Command name";
   }
 
+  /**
+   * A getter for the description of the command.
+   *
+   * @return the description of the command
+   */
   default String getDescription() {
     return "Description of a command";
   }
 
+  /**
+   * The code to be executed when a command is run.
+   */
   void execute();
 }
