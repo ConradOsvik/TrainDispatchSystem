b527e1929d3a1d4290d4c98ebff81a50e53ca44a
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 02:56:21 +0100

    feat(RegexValidator): added a simple pattern validation class

--- src/main/java/edu/ntnu/stud/utils/RegexValidator.java
+++ src/main/java/edu/ntnu/stud/utils/RegexValidator.java
@@ -1,2 +1,11 @@
-package edu.ntnu.stud.utils;public class RegexValidator {
+package edu.ntnu.stud.utils;
+
+public final class RegexValidator {
+  public static boolean isTime(String input) {
+    return input.matches("^([01]?[0-9]|2[0-3]):[0-5][0-9]$");
+  }
+
+  public static boolean isLine(String input) {
+    return input.matches("^[A-Z][0-9]{1,2}$");
+  }
 }
