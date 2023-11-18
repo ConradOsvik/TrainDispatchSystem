3c534198cc0d16fe10a0cd3d9d2b3d4c7bcae1d3
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:23 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/commands/SetTrainDelayCommand.java
+++ src/main/java/edu/ntnu/stud/commands/SetTrainDelayCommand.java
@@ -4,28 +4,49 @@ import edu.ntnu.stud.controllers.TrainController;
 import edu.ntnu.stud.input.ValidatedInput;
 import edu.ntnu.stud.views.ConsoleView;
 
+/**
+ * This class represents the command for adding a delay to a train.
+ */
 public class SetTrainDelayCommand implements Command {
 
   private final TrainController trainController;
   private final ConsoleView consoleView;
   private final ValidatedInput validatedInput;
 
+  /**
+   * Initializes the SetTrainDelayCommand.
+   *
+   * @param trainController The TrainController to use.
+   */
   public SetTrainDelayCommand(TrainController trainController) {
     this.trainController = trainController;
     this.consoleView = trainController.getConsoleView();
     this.validatedInput = new ValidatedInput();
   }
 
+  /**
+   * Returns the name of the command.
+   *
+   * @return the name of the command.
+   */
   @Override
   public String getName() {
     return "Add train delay";
   }
 
+  /**
+   * Returns the description of the command.
+   *
+   * @return the description of the command.
+   */
   @Override
   public String getDescription() {
     return "adds a delay to a train";
   }
 
+  /**
+   * executes the command.
+   */
   @Override
   public void execute() {
     consoleView.displayMessage("Please enter the train number:");
