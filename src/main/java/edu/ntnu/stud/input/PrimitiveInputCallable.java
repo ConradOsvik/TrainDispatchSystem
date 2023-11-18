6f4608e4bc8713a1d7c40ab17b0af5cab0e277a3
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:12 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/input/PrimitiveInputCallable.java
+++ src/main/java/edu/ntnu/stud/input/PrimitiveInputCallable.java
@@ -2,7 +2,19 @@ package edu.ntnu.stud.input;
 
 import edu.ntnu.stud.exceptions.InvalidFormatException;
 
+/**
+ * This class represents a callable method for primitive user input.
+ *
+ * @param <T> The type of the method.
+ */
 @FunctionalInterface
 public interface PrimitiveInputCallable<T> {
+
+  /**
+   * Calls the method.
+   *
+   * @return the result of the method
+   * @throws InvalidFormatException if the input has invalid format
+   */
   T call() throws InvalidFormatException;
 }
