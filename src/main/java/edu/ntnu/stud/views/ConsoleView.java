a18b30168a0ef6d529293a4e9910e21922208923
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 02:55:41 +0100

    feat(ConsoleView): added displayMessage method and updated getUserChoice

--- src/main/java/edu/ntnu/stud/views/ConsoleView.java
+++ src/main/java/edu/ntnu/stud/views/ConsoleView.java
@@ -1,26 +1,44 @@
 package edu.ntnu.stud.views;
 
 import edu.ntnu.stud.commands.Command;
-import edu.ntnu.stud.input.InputValidator;
+import edu.ntnu.stud.input.ValidatedInput;
+import edu.ntnu.stud.utils.Color;
 import java.util.List;
 
 public class ConsoleView {
 
-  private final InputValidator inputValidator;
+  private final ValidatedInput inputValidator;
 
   public ConsoleView() {
-    this.inputValidator = new InputValidator();
+    this.inputValidator = new ValidatedInput();
   }
 
   public void displayMenu(List<Command> commands) {
     System.out.println("Please choose a command:");
     for (int i = 0; i < commands.size(); i++) {
       System.out.println(
-          (i + 1) + ": " + commands.get(i).getName() + " - " + commands.get(i).getDescription());
+          Color.colorString(Integer.toString(i + 1), Color.WHITE) + ": " + Color.colorString(
+              commands.get(i).getName(), Color.CYAN) + " - " + commands.get(i).getDescription());
     }
   }
 
-  public String getUserInput() {
-    return inputValidator.getTime();
+  public void displayMessage(String message) {
+    System.out.println(message);
+  }
+
+  public int getUserInput(int commandsSize) {
+    try {
+      int choice = inputValidator.getInt();
+
+      if (choice < 1 || choice > commandsSize) {
+        throw new IllegalArgumentException(
+            String.format("Please enter a number between 1 and %d:", commandsSize));
+      }
+
+      return choice;
+    } catch (IllegalArgumentException e) {
+      this.displayMessage(Color.colorString(e.getMessage(), Color.RED));
+      return getUserInput(commandsSize);
+    }
   }
 }
