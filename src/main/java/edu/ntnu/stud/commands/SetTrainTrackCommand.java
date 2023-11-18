7d3adbdfe6d1c00c849f8bbec9cf3387c51539f6
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 02:50:26 +0100

    feat(Commands): added the command setting the train track

--- src/main/java/edu/ntnu/stud/commands/SetTrainTrackCommand.java
+++ src/main/java/edu/ntnu/stud/commands/SetTrainTrackCommand.java
@@ -1,2 +1,38 @@
-package edu.ntnu.stud.commands;public class setTrainTrackCommand {
+package edu.ntnu.stud.commands;
+
+import edu.ntnu.stud.controllers.TrainController;
+import edu.ntnu.stud.input.ValidatedInput;
+import edu.ntnu.stud.views.ConsoleView;
+
+public class SetTrainTrackCommand implements Command {
+
+  private final TrainController trainController;
+  private final ConsoleView consoleView;
+  private final ValidatedInput validatedInput;
+
+  public SetTrainTrackCommand(TrainController trainController) {
+    this.trainController = trainController;
+    this.consoleView = trainController.getConsoleView();
+    this.validatedInput = new ValidatedInput();
+  }
+
+  @Override
+  public String getName() {
+    return "Set train track";
+  }
+
+  @Override
+  public String getDescription() {
+    return "sets the track of a train";
+  }
+
+  @Override
+  public void execute() {
+    consoleView.displayMessage("Enter the train number:");
+    int trainNumber = validatedInput.getTrainNumber();
+    consoleView.displayMessage("Enter the track number:");
+    int track = validatedInput.getTrack();
+
+    trainController.setTrainTrackAndPrintMessage(trainNumber, track);
+  }
 }
