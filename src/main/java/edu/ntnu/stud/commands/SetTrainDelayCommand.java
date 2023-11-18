e89a9451e6343b2eb5fedfd0ed6e178e70305d73
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 02:49:48 +0100

    feat(Commands): added the command setting the departure delay of a train

--- src/main/java/edu/ntnu/stud/commands/SetTrainDelayCommand.java
+++ src/main/java/edu/ntnu/stud/commands/SetTrainDelayCommand.java
@@ -1,15 +1,19 @@
 package edu.ntnu.stud.commands;
 
 import edu.ntnu.stud.controllers.TrainController;
+import edu.ntnu.stud.input.ValidatedInput;
 import edu.ntnu.stud.views.ConsoleView;
 
-public class AddTrainDelayCommand implements Command {
+public class SetTrainDelayCommand implements Command {
+
   private final TrainController trainController;
   private final ConsoleView consoleView;
+  private final ValidatedInput validatedInput;
 
-  public AddTrainDelayCommand(TrainController trainController) {
+  public SetTrainDelayCommand(TrainController trainController) {
     this.trainController = trainController;
     this.consoleView = trainController.getConsoleView();
+    this.validatedInput = new ValidatedInput();
   }
 
   @Override
@@ -24,6 +28,11 @@ public class AddTrainDelayCommand implements Command {
 
   @Override
   public void execute() {
-    System.out.println("AddTrainDelayCommand executed");
+    consoleView.displayMessage("Please enter the train number:");
+    int trainNumber = validatedInput.getTrainNumber();
+    consoleView.displayMessage("Please enter the delay on format HH:mm:");
+    String delay = validatedInput.getTime();
+
+    trainController.setTrainDelayAndPrintMessage(trainNumber, delay);
   }
 }
