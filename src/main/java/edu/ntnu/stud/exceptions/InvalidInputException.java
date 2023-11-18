3f05d41c7d94990aa40af91a836c2a01f8eaa016
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:10 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/exceptions/InvalidInputException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidInputException.java
@@ -1,13 +1,28 @@
 package edu.ntnu.stud.exceptions;
 
+/**
+ * This class represents an exception thrown when the input is logically invalid.
+ */
 public class InvalidInputException extends Exception {
-  private static final String ERROR_MESSAGE = "The input you provided is invalid, please try again:";
 
-  public InvalidInputException(){
+  private static final String ERROR_MESSAGE = "The input you provided"
+      + " is invalid, please try again:";
+
+  /**
+   * Initializes the InvalidInputException calling super with the predefined error message to
+   * display.
+   */
+  public InvalidInputException() {
     super(ERROR_MESSAGE);
   }
 
-  public InvalidInputException(String message){
+  /**
+   * Initializes the InvalidInputException calling super with the provided error message to
+   * display.
+   *
+   * @param message The error message to display.
+   */
+  public InvalidInputException(String message) {
     super(message);
   }
 }
