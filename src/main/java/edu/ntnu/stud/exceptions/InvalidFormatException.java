3efd718cef980163bffb2ee5b5cacfd750a29c18
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:07 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/exceptions/InvalidFormatException.java
+++ src/main/java/edu/ntnu/stud/exceptions/InvalidFormatException.java
@@ -1,13 +1,28 @@
 package edu.ntnu.stud.exceptions;
 
+/**
+ * This class represents an exception thrown when the input has an invalid format.
+ */
 public class InvalidFormatException extends Exception {
-  private static final String ERROR_MESSAGE = "The input you provided has an invalid format, please try again:";
 
-  public InvalidFormatException(){
+  private static final String ERROR_MESSAGE = "The input you provided has an"
+      + " invalid format, please try again:";
+
+  /**
+   * Initializes the InvalidFormatException calling super with the predefined error message to
+   * display.
+   */
+  public InvalidFormatException() {
     super(ERROR_MESSAGE);
   }
 
-  public InvalidFormatException(String message){
+  /**
+   * Initializes the InvalidFormatException calling super with the provided error message to
+   * display.
+   *
+   * @param message The error message to display.
+   */
+  public InvalidFormatException(String message) {
     super(message);
   }
 }
