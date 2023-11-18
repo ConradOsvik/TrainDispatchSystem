a48a29cf38c663aef20fb55bcd4cd8b2414d8459
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 02:48:28 +0100

    feat(Commands): added a simple exit command

--- src/main/java/edu/ntnu/stud/commands/ExitCommand.java
+++ src/main/java/edu/ntnu/stud/commands/ExitCommand.java
@@ -1,2 +1,32 @@
-package edu.ntnu.stud.commands;public class exitCommand {
+package edu.ntnu.stud.commands;
+
+import edu.ntnu.stud.controllers.TrainController;
+import edu.ntnu.stud.utils.Color;
+import edu.ntnu.stud.views.ConsoleView;
+
+public class ExitCommand implements Command {
+
+  private final TrainController trainController;
+  private final ConsoleView consoleView;
+
+  public ExitCommand(TrainController trainController) {
+    this.trainController = trainController;
+    this.consoleView = trainController.getConsoleView();
+  }
+
+  @Override
+  public String getName() {
+    return "Exit";
+  }
+
+  @Override
+  public String getDescription() {
+    return "exits the application";
+  }
+
+  @Override
+  public void execute() {
+    System.out.println(Color.colorString("Exiting the application",Color.GREEN));
+    System.exit(0);
+  }
 }
