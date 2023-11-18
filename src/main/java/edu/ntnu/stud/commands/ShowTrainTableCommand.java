c344166bc3ae49f525861f14e82d6ea7bfb8d445
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 02:50:59 +0100

    feat(Commands): added a command for printing the register into a table

--- src/main/java/edu/ntnu/stud/commands/ShowTrainTableCommand.java
+++ src/main/java/edu/ntnu/stud/commands/ShowTrainTableCommand.java
@@ -1,2 +1,27 @@
-package edu.ntnu.stud.commands;public class showTrainTableCommand {
+package edu.ntnu.stud.commands;
+
+import edu.ntnu.stud.controllers.TrainController;
+
+public class ShowTrainTableCommand implements Command {
+
+  private final TrainController trainController;
+
+  public ShowTrainTableCommand(TrainController trainController) {
+    this.trainController = trainController;
+  }
+
+  @Override
+  public String getName() {
+    return "Show train table";
+  }
+
+  @Override
+  public String getDescription() {
+    return "shows a table of all train departures";
+  }
+
+  @Override
+  public void execute() {
+    trainController.showTableOfTrains();
+  }
 }
