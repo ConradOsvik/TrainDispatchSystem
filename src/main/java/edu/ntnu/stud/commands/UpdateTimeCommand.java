d063ec75b1dcbcdbe8b2d9d2c38eba152f7588de
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:39 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/commands/UpdateTimeCommand.java
+++ src/main/java/edu/ntnu/stud/commands/UpdateTimeCommand.java
@@ -4,28 +4,49 @@ import edu.ntnu.stud.controllers.TrainController;
 import edu.ntnu.stud.input.ValidatedInput;
 import edu.ntnu.stud.views.ConsoleView;
 
+/**
+ * This class represents the command for updating the time of the day.
+ */
 public class UpdateTimeCommand implements Command {
 
   private final TrainController trainController;
   private final ConsoleView consoleView;
   private final ValidatedInput validatedInput;
 
+  /**
+   * Initializes the UpdateTimeCommand.
+   *
+   * @param trainController The TrainController to use.
+   */
   public UpdateTimeCommand(TrainController trainController) {
     this.trainController = trainController;
     this.consoleView = trainController.getConsoleView();
     this.validatedInput = new ValidatedInput();
   }
 
+  /**
+   * Returns the name of the command.
+   *
+   * @return the name of the command.
+   */
   @Override
   public String getName() {
     return "Update time";
   }
 
+  /**
+   * Returns the description of the command.
+   *
+   * @return the description of the command.
+   */
   @Override
   public String getDescription() {
     return "updates the time of the day";
   }
 
+  /**
+   * executes the command.
+   */
   @Override
   public void execute() {
     consoleView.displayMessage("Please enter the time you want to set:");
