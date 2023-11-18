1787f12b198dcdc3f12860034e4fdb305426e08f
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:18 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/commands/SearchTrainByTrainNumberCommand.java
+++ src/main/java/edu/ntnu/stud/commands/SearchTrainByTrainNumberCommand.java
@@ -4,28 +4,49 @@ import edu.ntnu.stud.controllers.TrainController;
 import edu.ntnu.stud.input.ValidatedInput;
 import edu.ntnu.stud.views.ConsoleView;
 
+/**
+ * This class represents the command for searching for a train by its train number.
+ */
 public class SearchTrainByTrainNumberCommand implements Command {
 
   private final TrainController trainController;
   private final ConsoleView consoleView;
   private final ValidatedInput validatedInput;
 
+  /**
+   * Initializes the SearchTrainByTrainNumberCommand.
+   *
+   * @param trainController The TrainController to use.
+   */
   public SearchTrainByTrainNumberCommand(TrainController trainController) {
     this.trainController = trainController;
     this.consoleView = trainController.getConsoleView();
     this.validatedInput = new ValidatedInput();
   }
 
+  /**
+   * Returns the name of the command.
+   *
+   * @return the name of the command.
+   */
   @Override
   public String getName() {
     return "Get train by number";
   }
 
+  /**
+   * Returns the description of the command.
+   *
+   * @return the description of the command.
+   */
   @Override
   public String getDescription() {
     return "gets a train by its unique train number";
   }
 
+  /**
+   * executes the command.
+   */
   @Override
   public void execute() {
     consoleView.displayMessage("Please enter the train number you want to look up:");
