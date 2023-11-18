364168736e1c06a0560bfeb252cb98c2c2af8e14
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:35 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -11,16 +11,27 @@ public class TrainDispatchApp {
 
   private static TrainController trainController;
 
+  /**
+   * Initializes the TrainRegister, ConsoleView and TrainController.
+   */
   private static void init() {
     TrainRegister trainRegister = new TrainRegister();
     ConsoleView consoleView = new ConsoleView();
     trainController = new TrainController(trainRegister, consoleView);
   }
 
+  /**
+   * Starts the application by pointing it to the TrainController request handler.
+   */
   private static void start() {
     trainController.handleRequest();
   }
 
+  /**
+   * The main method of the application.
+   *
+   * @param args The command line arguments.
+   */
   public static void main(String[] args) {
     init();
 
