031acbddbe670df82f9507047befd7625a649b03
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:01 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/commands/ExitCommand.java
+++ src/main/java/edu/ntnu/stud/commands/ExitCommand.java
@@ -4,29 +4,50 @@ import edu.ntnu.stud.controllers.TrainController;
 import edu.ntnu.stud.utils.Color;
 import edu.ntnu.stud.views.ConsoleView;
 
+/**
+ * This class represents the command for exiting the application.
+ */
 public class ExitCommand implements Command {
 
   private final TrainController trainController;
   private final ConsoleView consoleView;
 
+  /**
+   * Initializes the ExitCommand.
+   *
+   * @param trainController The TrainController to use.
+   */
   public ExitCommand(TrainController trainController) {
     this.trainController = trainController;
     this.consoleView = trainController.getConsoleView();
   }
 
+  /**
+   * Returns the name of the command.
+   *
+   * @return the name of the command.
+   */
   @Override
   public String getName() {
     return "Exit";
   }
 
+  /**
+   * Returns the description of the command.
+   *
+   * @return the description of the command.
+   */
   @Override
   public String getDescription() {
     return "exits the application";
   }
 
+  /**
+   * executes the command.
+   */
   @Override
   public void execute() {
-    System.out.println(Color.colorString("Exiting the application",Color.GREEN));
+    System.out.println(Color.colorString("Exiting the application", Color.GREEN));
     System.exit(0);
   }
 }
