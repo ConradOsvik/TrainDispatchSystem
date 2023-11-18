accf01aa92ec3dc077754340681cecc5ae556825
conradosvik <conrad.tinius.osvik@gmail.com>
2023-11-18 17:48:03 +0100

    chore(JavaDoc): added javadoc comments

--- src/main/java/edu/ntnu/stud/input/InputCallable.java
+++ src/main/java/edu/ntnu/stud/input/InputCallable.java
@@ -3,8 +3,20 @@ package edu.ntnu.stud.input;
 import edu.ntnu.stud.exceptions.InvalidFormatException;
 import edu.ntnu.stud.exceptions.InvalidInputException;
 
+/**
+ * This class represents a callable method for user input.
+ *
+ * @param <T> The type of the method.
+ */
 @FunctionalInterface
 public interface InputCallable<T> {
 
+  /**
+   * Calls the method.
+   *
+   * @return the result of the method
+   * @throws InvalidFormatException if the input has invalid format
+   * @throws InvalidInputException  if the input is logically invalid
+   */
   T call() throws InvalidFormatException, InvalidInputException;
 }
