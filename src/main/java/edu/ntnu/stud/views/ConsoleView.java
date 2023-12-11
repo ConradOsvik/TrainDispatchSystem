d3ccf1cf0750f521514cde20e6bed2d589ea7ab6
conradosvik <conrad.tinius.osvik@gmail.com>
2023-12-11 03:27:03 +0100

    feat(ConsoleView): Decoupled ui methods from TrainController

--- src/main/java/edu/ntnu/stud/views/ConsoleView.java
+++ src/main/java/edu/ntnu/stud/views/ConsoleView.java
@@ -1,8 +1,12 @@
 package edu.ntnu.stud.views;
 
+import static edu.ntnu.stud.utils.Truncate.truncate;
+
 import edu.ntnu.stud.commands.Command;
 import edu.ntnu.stud.input.ValidatedInput;
+import edu.ntnu.stud.models.TrainDeparture;
 import edu.ntnu.stud.utils.Color;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,7 +20,79 @@ public class ConsoleView {
    * The constructor of the class, initializing a validated input.
    */
   public ConsoleView() {
-    this.inputValidator = new ValidatedInput();
+    this.inputValidator = new ValidatedInput(this);
+  }
+
+  /**
+   * Creates a table header.
+   */
+  private String header() {
+    return Color.colorString(
+        String.format(
+            "%-20s | %-20s | %-20s | %-20s | %-20s | %-20s",
+            truncate("Departure time"),
+            truncate("Line"),
+            truncate("Train number"),
+            truncate("Destination"),
+            truncate("Track"),
+            truncate("Delay")
+        ),
+        Color.YELLOW
+    );
+  }
+
+  /**
+   * Creates a table separator.
+   */
+  private String separator() {
+    return Color.colorString(String.join("", Collections.nCopies(header().length(), "-")),
+        Color.YELLOW);
+  }
+
+  /**
+   * Creates a table row using a train departure.
+   *
+   * @param trainDeparture the train departure.
+   */
+  private String row(TrainDeparture trainDeparture) {
+    return Color.colorString(String.format("%-20s | %-20s | %-20s | %-20s | %-20s | %-20s",
+            truncate(trainDeparture.getDepartureTime().toString()),
+            truncate(trainDeparture.getLine()),
+            truncate(Integer.toString(trainDeparture.getTrainNumber())),
+            truncate(trainDeparture.getDestination()),
+            truncate(
+                trainDeparture.getTrack() > 0
+                    ? Integer.toString(trainDeparture.getTrack())
+                    : "Unset"
+            ),
+            truncate(trainDeparture.getDelay().toString())
+        ),
+        Color.YELLOW
+    );
+  }
+
+  /**
+   * Prints a table of trains using a train departure.
+   *
+   * @param trainDeparture the train departure.
+   */
+  public void printTableOfTrains(TrainDeparture trainDeparture) {
+    this.displayMessage(header());
+    this.displayMessage(separator());
+    this.displayMessage(row(trainDeparture));
+  }
+
+  /**
+   * Prints a table of trains using a list of train departures.
+   *
+   * @param trainDepartures the list of train departures.
+   */
+  public void printTableOfTrains(List<TrainDeparture> trainDepartures) {
+    this.displayMessage(header());
+    trainDepartures.forEach(trainDeparture -> {
+      this.displayMessage(separator());
+      this.displayMessage(row(trainDeparture));
+    });
   }
 
   /**
@@ -31,15 +107,6 @@ public class ConsoleView {
           Color.colorString(Integer.toString(i + 1), Color.WHITE) + ": " + Color.colorString(
               commands.get(i).getName(), Color.CYAN) + " - " + commands.get(i).getDescription());
     }
-  }
-
-  /**
-   * A method for displaying a message to the user.
-   *
-   * @param message the message to be displayed
-   */
-  public void displayMessage(String message) {
-    System.out.println(message);
   }
 
   /**
@@ -62,5 +129,18 @@ public class ConsoleView {
       this.displayMessage(Color.colorString(e.getMessage(), Color.RED));
       return getUserInput(commandsSize);
     }
+  }
+
+  /**
+   * A method for displaying a message to the user.
+   *
+   * @param message the message to be displayed
+   */
+  public void displayMessage(String message) {
+    System.out.println(message);
+  }
+
+  public void displayErrorMessage(String message) {
+    System.err.println(message);
   }
 }
