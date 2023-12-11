e35c66499effea438b9a8c52cbca7943dfb0017d
conradosvik <conrad.tinius.osvik@gmail.com>
2023-12-11 10:54:27 +0100

    feat(TestTrainController): Added missing tests

--- src/test/java/edu/ntnu/stud/controllers/TestTrainController.java
+++ src/test/java/edu/ntnu/stud/controllers/TestTrainController.java
@@ -1,8 +1,9 @@
 package edu.ntnu.stud.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.ntnu.stud.models.TrainDeparture;
 import edu.ntnu.stud.models.TrainRegister;
@@ -17,16 +18,12 @@ class TestTrainController {
   private TrainRegister trainRegister;
   private ConsoleView consoleView;
   private TrainController trainController;
-  private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-  private final PrintStream standardOut = System.out;
 
   @BeforeEach
   void setUp() {
     trainRegister = new TrainRegister();
     consoleView = new ConsoleView();
     trainController = new TrainController(trainRegister, consoleView);
-    System.setOut(new PrintStream(outputStream));
   }
 
   @Test
@@ -38,8 +35,6 @@ class TestTrainController {
 
   @Test
   void addTrainToRegister_TrainIsAddedToRegister_Succeeds() {
-    String successMessage = "";
-
     TrainDeparture trainDeparture = new TrainDeparture(
         "12:00",
         "F1",
@@ -49,14 +44,11 @@ class TestTrainController {
         "00:00"
     );
 
-    trainController.addTrainToRegister(trainDeparture);
-    String output = outputStream.toString().trim();
-
-    //assertEquals();
+    assertTrue(trainController.addTrainToRegister(trainDeparture));
   }
 
   @Test
-  void addTrainToRegister_TrainAlreadyExistsInRegister_ThrowsException() {
+  void addTrainToRegister_TrainAlreadyExistsInRegister_Fails() {
     TrainDeparture trainDeparture = new TrainDeparture(
         "12:00",
         "F1",
@@ -77,6 +69,232 @@ class TestTrainController {
 
     trainController.addTrainToRegister(trainDeparture);
 
-    assertThrows(IllegalArgumentException.class, () -> trainController.addTrainToRegister(newTrainDeparture));
+    assertFalse(trainController.addTrainToRegister(newTrainDeparture));
+  }
+
+  @Test
+  void addTrainToRegister_TrainIsNull_Fails() {
+    TrainDeparture trainDeparture = null;
+
+    assertFalse(trainController.addTrainToRegister(trainDeparture));
+  }
+
+  @Test
+  void setTrainTrack_TrackIsUpdated_Succeeds(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertTrue(trainController.setTrainTrack(100, 2));
+  }
+
+  @Test
+  void setTrainTrack_TrainDoesNotExist_Fails(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertFalse(trainController.setTrainTrack(101, 2));
+  }
+
+  @Test
+  void setTrainTrack_TrackIsNegative_Fails(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertFalse(trainController.setTrainTrack(100, -2));
+  }
+
+  @Test
+  void setTrainDelay_DelayIsUpdated_Succeeds(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertTrue(trainController.setTrainDelay(100, "00:10"));
+  }
+
+  @Test
+  void setTrainDelay_DelayIsOutOfRange_Fails(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertFalse(trainController.setTrainDelay(100, "25:00"));
+  }
+
+  @Test
+  void setTrainDelay_TrainDoesNotExist_Fails(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertFalse(trainController.setTrainDelay(101, "00:10"));
+  }
+
+  @Test
+  void searchTrainByTrainNumber_TrainIsFound_Succeeds(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertTrue(trainController.searchTrainByTrainNumber(100));
+  }
+
+  @Test
+  void searchTrainByTrainNumber_TrainIsNotFound_Fails(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertFalse(trainController.searchTrainByTrainNumber(101));
+  }
+
+  @Test
+  void searchTrainsByDestination_TrainsAreFound_Succeeds(){
+    TrainDeparture trainDeparture1 = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+        );
+
+    TrainDeparture trainDeparture2 = new TrainDeparture(
+        "12:00",
+        "F1",
+        101,
+        "Oslo",
+        1,
+        "00:00"
+        );
+
+    trainController.addTrainToRegister(trainDeparture1);
+    trainController.addTrainToRegister(trainDeparture2);
+
+    assertTrue(trainController.searchTrainsByDestination("Oslo"));
+  }
+
+  @Test
+  void searchTrainsByDestination_TrainsAreNotFound_Fails(){
+    TrainDeparture trainDeparture1 = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+        );
+
+    TrainDeparture trainDeparture2 = new TrainDeparture(
+        "12:00",
+        "F1",
+        101,
+        "Oslo",
+        1,
+        "00:00"
+        );
+
+    trainController.addTrainToRegister(trainDeparture1);
+    trainController.addTrainToRegister(trainDeparture2);
+
+    assertFalse(trainController.searchTrainsByDestination("Trondheim"));
+  }
+
+  @Test
+  void updateTime_UpdatesTime_Succeeds(){
+    boolean time = trainController.updateTime("12:00");
+
+    assertTrue(time);
+  }
+
+  @Test
+  void updateTime_TimeIsBefore_Fails(){
+    trainController.updateTime("12:00");
+
+    boolean time = trainController.updateTime("11:00");
+
+    assertFalse(time);
+  }
+
+  @Test
+  void updateTime_TimeIsOutOfRange_Fails(){
+    boolean time = trainController.updateTime("25:00");
+
+    assertFalse(time);
+  }
+
+  @Test
+  void showTableOfTrains_Succeeds(){
+    TrainDeparture trainDeparture = new TrainDeparture(
+        "12:00",
+        "F1",
+        100,
+        "Oslo",
+        1,
+        "00:00"
+    );
+
+    trainController.addTrainToRegister(trainDeparture);
+
+    assertTrue(trainController.showTableOfTrains());
   }
 }
